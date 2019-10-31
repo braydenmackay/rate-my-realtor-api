71fd086b59d70941f6a556e2b282dd86332d3461
braydenmackay <braydenmackay1@gmail.com>
2019-10-31 12:03:18 -0600

    fixed marshmallow import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask-marshmallow import Marshmallow
+from flask_marshmallow import Marshmallow
 from flask_heroku import Heroku
 
 app = Flask(__name__)
